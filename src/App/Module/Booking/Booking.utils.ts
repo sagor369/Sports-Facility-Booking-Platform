eb9a82682e84c4d0c90fd43d98a75030f4d99f72
Sagor <sagor.heaking@gmail.com>
2024-06-17 20:51:54 +0600

    all route complete

--- src/App/Module/Booking/Booking.utils.ts
+++ src/App/Module/Booking/Booking.utils.ts
@@ -2,14 +2,15 @@ import { TBooking } from "./Booking.interface";
 import { Booking } from "./Booking.model";
 
 export const TimeCheck = async (payload: TBooking) => {
-  const payloadDate = new Date(payload.date)
+  const payloadDate = payload.date
   const bookingData = await Booking.findOne({ faculty: payload.faculty, date: payloadDate });
   const newStartTime = `2024-02-12T${payload?.startTime}`;
-  const newEndTime = `2024-02-12TT${payload?.endTime}`;
-  const oldStartTime = `2024-02-12TT${bookingData?.startTime}`;
-  const oldEndTime = `2024-02-12TT${bookingData?.endTime}`;
+  const newEndTime = `2024-02-12T${payload?.endTime}`;
+  const oldStartTime = `2024-02-12T${bookingData?.startTime}`;
+  const oldEndTime = `2024-02-12T${bookingData?.endTime}`;
 
   if (newStartTime < oldEndTime && newEndTime > oldStartTime) {
     return true;
   }
+  return false
 };
